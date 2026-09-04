351a8518815a176dcbd7af394a716cc1c8edbc1c
Arthurrmande <arthur.mande@etu.unilim.fr>
2026-09-04 11:21:50 +0200

    #td1

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+function App() {
+  
+  return (
+  <> 
+    <h1> Hedy Lamarr's Todos </h1>
+
+    <img src="https://i.imgur.com/yXOvdOSs.jpg" alt="img Hedy" />
+
+    <ul>
+      <li>Invent new traffic lights </li> 
+      <li>Rehearse a movie scene </li>
+      <li>Improve the spectrum technology </li>
+    </ul>
+
+  </>
+  )
+
+}
+
+export default App
